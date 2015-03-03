9172f909ba05ffa05611c952f078b12060584776
Adam Bodie <adam.bodie@yahoo.com>
2015-03-02 20:34:23 -0800

    Added guards to convert words if first or all letters are symbols.

--- piglatin.hs
+++ piglatin.hs
@@ -11,6 +11,7 @@ import Data.Char
 -- Sets up the first and last letters of a word
 firstLetter :: [a] -> a
 firstLetter xs = xs !! 0
+lastLetter :: [a] -> a
 lastLetter xs = xs !! (length xs - 1)
 
 -- A list of all the letters of the alphabet considered vowels.  For simplicity sakes, y is included as a vowel.
@@ -29,13 +30,18 @@ characters = map chr [0..127]
 symbols = snd (partition (`elem` allLetters) characters)
 
 -- Separates the word and the symbol at the end of a word if found
-firstSymbol = fst . break (`elem` symbols)
-secondSymbol = snd . break (`elem` symbols)  
+firstSymbolEnd = fst . break (`elem` symbols)
+secondSymbolEnd = snd . break (`elem` symbols)  
+
+-- Separates the word and the symbol at the beginning of a word if found
+firstSymbolBegin = fst . span (`elem` symbols)
+secondSymbolBegin = snd . span (`elem` symbols)
 
 -- Checks to determine if the first letter in a word is a vowel, adding +"way" at the end if it is, keeping it the same if not.
 pigVowel :: String -> String
 pigVowel myWord 
-		| firstLetter myWord `elem` allVowels = map toLower (firstSymbol myWord) ++ "way" ++ secondSymbol myWord
+		| firstLetter myWord `elem` symbols = firstSymbolBegin (myWord) ++ map toLower (firstSymbolEnd $ secondSymbolBegin myWord) ++ "way" ++ secondSymbolEnd (secondSymbolBegin (myWord)) 
+		| firstLetter myWord `elem` allVowels = map toLower (firstSymbolEnd myWord) ++ "way" ++ secondSymbolEnd myWord
 		| otherwise = map toLower myWord
 
 -- Searches in a word until the first vowel appears, separating the consonant cluster before the vowel from the rest of the word.				
@@ -44,8 +50,10 @@ secondCluster = snd . span (`elem` consonants)
 
 -- Check to determine of the first letter in a word is a consonant, grabbing the consonant cluster and putting it after the rest of the word, suffix "ay" and any symbols at the end of the word
 pigWord :: String -> String
-pigWord myWord 
-		| firstLetter myWord `elem` consonants = firstSymbol (secondCluster myWord) ++ map toLower (firstCluster myWord) ++ "ay" ++ secondSymbol myWord
+pigWord myWord
+		| all (`notElem` allLetters) myWord == True = myWord
+		| firstLetter myWord `elem` symbols = firstSymbolBegin (myWord) ++ firstSymbolEnd (secondCluster (tail myWord)) ++ map toLower (firstCluster (tail myWord)) ++ "ay" ++ secondSymbolEnd (secondSymbolBegin (myWord))
+		| firstLetter myWord `elem` consonants = firstSymbolEnd (secondCluster myWord) ++ map toLower (firstCluster myWord) ++ "ay" ++ secondSymbolEnd myWord
 		| otherwise = pigVowel myWord
 
 -- Takes a sentence, separating it into a list of words, turning words into Pig Latin and making the list a sentence again.				
